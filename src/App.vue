8ab53ea97ed90a044889e397f09c73a9e5849e47
jmsaulnier <jmsaulnier.dev@gmail.com>
2016-01-07 21:10:58 +0100

    bam

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './display/components/Hello.vue'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
